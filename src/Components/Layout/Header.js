d6ec3ec30b4c1c2ae76ddd7e06457ea8e15dac26
Misiogrzanka <77200161+Misiogrzanka@users.noreply.github.com>
2021-07-17 20:51:08 +0200

    Finished the app (added logic, Context, Reducer, Effect and Ref)

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -4,12 +4,12 @@ import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 
-const Header = () => {
+const Header = (props) => {
 	return (
 		<React.Fragment>
 			<header className={classes.header}>
 				<h1>Orderly</h1>
-				<HeaderCartButton />
+				<HeaderCartButton onClick={props.onShowCart} />
 			</header>
 			<div className={classes['main-image']}>
 				<img src={mealsImage} alt='Table full of yummy food' />
